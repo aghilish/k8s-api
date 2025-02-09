c1390b4ac88ca176319972d21916cbfa04cc98b6
aghilish <aghilish@users.noreply.github.com>
2025-02-09 14:16:31 +0000

    deploy: 396c234fdc34d4d119fba3f0a5690c8337254ef9

--- assets/slidev/default-aFVfm64h.js
+++
@@ -1,1 +0,0 @@
-import{o,b as t,f as s}from"../modules/vue-DfOUTzrx.js";import{_ as r}from"../index-tpG3ARvq.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
