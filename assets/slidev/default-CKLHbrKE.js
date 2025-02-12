4b1d58c6b6dc86ca4b5e6a89e1d35346789638c5
aghilish <aghilish@users.noreply.github.com>
2025-02-12 11:08:06 +0000

    deploy: f6f69e54b451a8d28c6264b0d11bd8de5e0aee1d

---
+++ assets/slidev/default-CKLHbrKE.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-DvR9HuUl.js";import{_ as r}from"../index-DG9xwnuz.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
