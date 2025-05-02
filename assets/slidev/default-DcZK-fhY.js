f887eb0fe279116abc4f3e8d453d6dc7f55e5663
aghilish <aghilish@users.noreply.github.com>
2025-05-02 10:07:44 +0000

    deploy: abc56371592f17bb9da05073cfb4dd9a6adcfda1

---
+++ assets/slidev/default-DcZK-fhY.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-Dyu_1XSi.js";import{_ as r}from"../index-AE2WFcrj.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
