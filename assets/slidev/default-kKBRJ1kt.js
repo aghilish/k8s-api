ffc141ccc6a564749ef02e3cde79b5d7cfa91899
aghilish <aghilish@users.noreply.github.com>
2025-05-02 10:43:46 +0000

    deploy: 985c002f22846bb2de906da41a81e007b34db574

--- assets/slidev/default-kKBRJ1kt.js
+++
@@ -1,1 +0,0 @@
-import{o,b as t,f as s}from"../modules/vue-Dyu_1XSi.js";import{_ as r}from"../index-BDkfWsJH.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
